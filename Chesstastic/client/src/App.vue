972c52d73b181ff6f7ab0440bdf2dd64734272ba
Christoffer <jo.christoffer@gmail.com>
2018-12-28 11:05:06 +0100

    added some text to layout

--- Chesstastic/client/src/App.vue
+++ Chesstastic/client/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
   <TheChessboard />
+    <SeeksComponentLayout />
   </div>
 
 </template>
 
 <script>
 import TheChessboard from './components/TheChessboard.vue'
+import SeeksComponentLayout from './components/SeeksComponentLayout'
 
 export default {
   name: 'app',
   components: {
+      SeeksComponentLayout,
     TheChessboard,
 
   }
---
+++ Chesstastic/client/src/components/SeeksComponentLayout.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container-seekComponent">
+        <h2>Seek component!</h2>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SeeksComponentLayout"
+    }
+    
+</script>
+
+<style scoped>
+
+    .container-seekComponent {
+        display: flex;
+        margin: 25px;
+    }
+
+</style>
