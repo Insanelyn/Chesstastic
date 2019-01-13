3189375624291f14b7f8d4a37a8b4a57a9b2ce61
Insanelyn <evelynjepsen@gmail.com>
2019-01-13 20:57:26 +0100

    added quickplay content with routing ready for each tile,also tried to clean up different sizes on lobby

--- Chesstastic/client/src/App.vue
+++ Chesstastic/client/src/App.vue
@@ -4,7 +4,6 @@
 
     <TheHeader />
     <SeekComponentLayout />
-    <router-view />
 
 
   </div>
--- Chesstastic/client/src/lobby/components/SnabbParningContent.vue
+++ Chesstastic/client/src/lobby/components/SnabbParningContent.vue
@@ -1,22 +1,117 @@
 <template>
-    <div>
-        this is also some content for the upcoming pages.
+  <div class="container">
+    <div class="square column-1">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>1+0</h2>
+        <h3>Bullet</h3>
+      </router-link>
     </div>
+
+    <div class="square column-2">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>2+1</h2>
+        <h3>Bullet</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-3">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>3+0</h2>
+        <h3>Blitz</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-1-row-2">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>3+2</h2>
+        <h3>Blitz</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-2-row-2">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>5+0</h2>
+        <h3>Blitz</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-3-row-2">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>5+3</h2>
+        <h3>Blitz</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-1-row-3">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>10+0</h2>
+        <h3>Rapid</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-2-row-3">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h2>15+15</h2>
+        <h3>Classical</h3>
+      </router-link>
+    </div>
+
+    <div class="square column-3-row-3">
+      <router-link class="quickplay" tag="a" :to="{name: 'SnabbParning',}">
+        <h3>Anpassad</h3>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "SnabbParningContent"
-    }
-
+export default {
+  data() {
+    return {};
+  },
+  created() {}
+};
 </script>
 
 <style scoped>
+.container {
+  text-align: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: auto;
+  box-sizing: border-box;
+}
 
-    div {
-        text-align: center;
-        margin-top: 25px;
-    }
+.column-1,
+.column-2,
+.column-3 {
+  padding: 50px;
+}
 
+.column-1-row-2,
+.column-2-row-2,
+.column-3-row-2 {
+  grid-column: span 1;
+  padding: 50px;
+}
 
+.column-1-row-3,
+.column-2-row-3,
+.column-3-row-3 {
+  grid-column: span 1;
+  padding: 50px;
+}
+
+.square{
+    background-color: white;
+    border: 1px solid black;
+
+}
+.square:hover{
+    background-color: lightskyblue;
+}
+.quickplay{
+    text-decoration: none;
+    color:black;
+}
 </style>
